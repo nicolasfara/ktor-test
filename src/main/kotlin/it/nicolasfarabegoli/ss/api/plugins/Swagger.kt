8aa04201c20888a3cfb281059aad2e4ca50ff8d8
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-12-10 15:36:16 +0100

    feat(openapi): add openapi route example

--- src/main/kotlin/it/nicolasfarabegoli/ss/api/plugins/Swagger.kt
+++ src/main/kotlin/it/nicolasfarabegoli/ss/api/plugins/Swagger.kt
@@ -1,12 +1,32 @@
 package it.nicolasfarabegoli.ss.api.plugins
 
 import com.papsign.ktor.openapigen.OpenAPIGen
+import com.papsign.ktor.openapigen.schema.namer.DefaultSchemaNamer
+import com.papsign.ktor.openapigen.schema.namer.SchemaNamer
 import io.ktor.application.*
-import io.ktor.application.*
+import kotlin.reflect.KType
 
 fun Application.configureSwagger() {
     install(OpenAPIGen) {
-        serveSwaggerUi = true
-        swaggerUiPath = "/swagger-ui"
+        info {
+            version = "0.1.0"
+            title = "Smart IoT Shelf"
+            description = "TODO"
+            contact {
+                name = "Nicolas Farabegoli"
+                email = "nicolas.farabegoli@gmail.com"
+            }
+        }
+        server("http://localhost:8080/") {
+            description = "TODO"
+        }
+        //rename DTOs from java type name to generator compatible form
+        replaceModule(DefaultSchemaNamer, object : SchemaNamer {
+            val regex = Regex("[A-Za-z0-9_.]+")
+
+            override fun get(type: KType): String {
+                return type.toString().replace(regex) { it.value.split(".").last() }.replace(Regex(">|<|, "), "_")
+            }
+        })
     }
 }
